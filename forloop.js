e5062a56b7edeed1744172ba52459cd84e56e482
sarang-kharpate <sarang.kharpate@gmail.com>
2020-06-12 10:04:54 +0530

    Basic For Loop Tutorial

---
+++ forloop.js
@@ -0,0 +1,31 @@
+// for loop
+//
+// for (statement1; statement2; statement3) {
+//   //Execute some code
+// }
+//
+// //Sample for loop in Javascripts looks like below
+// for (var i = 0; i < array.length; i++) {
+//   array[i]
+// }
+//
+// Statement1 starts before the loop begins;
+// statement2 defines the condition for ForLoop;
+// statement3 Runs after the loop ends
+
+//Example one :
+// for (var i = 0; i < 5; i++) {
+//   console.log("Number is :"+i);
+// }
+
+//Example 2;
+//
+// var words='ABCDEFGHIJKLMNOPQRSTUVWXYX'
+//
+// for (var i = 0; i < words.length; i++) {
+//  if (i%2 === 0) {
+//   console.log('Even placed words are '+words[i]);
+//  }
+// }
+
+Example 3
