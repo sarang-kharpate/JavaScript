6aed7c29677b5f83b441d5eaf3417a3e8e088420
sarang-kharpate <sarang.kharpate@gmail.com>
2020-06-12 11:45:40 +0530

    Practive Project

--- forloop.js
+++ forloop.js
@@ -14,18 +14,70 @@
 // statement3 Runs after the loop ends
 
 //Example one :
-// for (var i = 0; i < 5; i++) {
-//   console.log("Number is :"+i);
-// }
+for (var i = 0; i < 5; i++) {
+  console.log("Number is :"+i);
+}
 
 //Example 2;
 //
-// var words='ABCDEFGHIJKLMNOPQRSTUVWXYX'
-//
-// for (var i = 0; i < words.length; i++) {
-//  if (i%2 === 0) {
-//   console.log('Even placed words are '+words[i]);
-//  }
-// }
+var words='ABCDEFGHIJKLMNOPQRSTUVWXYX'
 
-Example 3
+for (var i = 0; i < words.length; i++) {
+ if (i%2 === 0) {
+  console.log('Even placed words are '+words[i]);
+ }
+}
+
+/// PART 8 - LOOP EXERCISES
+// Before we continue on with your project, let's practice some loops!
+// You'll have just two problems, but you'll need to use each loop type we
+// learned about to solve them!
+
+
+///////////////////
+//// PROBLEM 1 ///
+/////////////////
+
+// Use a For Loop to print (console.log()) out the word "hello" 5 times.
+//
+// Do this with a While Loop and a For Loop
+
+While Loop
+var i = 0;
+
+while (i < 5) {
+  console.log('Hello');
+  i+=1;
+}
+
+
+// For Loop
+for (var i = 0; i < 5; i++) {
+  console.log('Hello');
+}
+
+
+
+/////////////////
+// PROBLEM 2 ///
+///////////////
+
+// Use Loops to console.log() (print out) all the odd numbers from 1 to 25
+// Do this using two methods, a while loop and a for loop
+
+// METHOD ONE
+// While Loop
+var i = 1
+while (i < 26) {
+  if (i%2 != 0){
+    console.log(i);
+  }
+  i+=1;
+}
+
+
+// METHOD TWO
+// For Loop
+for (var i = 1; i < 26; i+=2) {
+  console.log(i);
+}
